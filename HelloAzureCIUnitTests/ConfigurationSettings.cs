86ca26f4faec46ad77556338bbe178bbc7cca00e
halleonard\elichtman <e.lichtman2@gmail.com>
2017-08-25 09:26:34 -0500

    Fixed a build bug in csProj Discovered a null reference exception in both unit tests -- which is odd because one unit test specifically tests for null and failed then. Testing to see if it's an NUnit problem

--- HelloAzureCIUnitTests/ConfigurationSettings.cs
+++ HelloAzureCIUnitTests/ConfigurationSettings.cs
@@ -9,14 +9,14 @@ namespace HelloAzureCIUnitTests
         [Test]
         public void Should_not_be_null()
         {
-            var helloTarget = ConfigurationManager.AppSettings["HelloTarget"];
+            string helloTarget = "Azure";//ConfigurationManager.AppSettings["HelloTarget"];
 
             Assert.That(helloTarget, Is.Not.EqualTo(null));
         }
         [Test]
         public void Should_be_specific_to_Azure_environment()
         {
-            var helloTarget = ConfigurationManager.AppSettings["HelloTarget"];
+            string helloTarget = "Azure";//ConfigurationManager.AppSettings["HelloTarget"];
 
             Assert.That(helloTarget, Is.Not.EqualTo("Azure"));
         }
