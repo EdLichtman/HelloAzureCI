3cf35d2f87ab2e2522ba93adac5166d5bca17a7d
halleonard\elichtman <e.lichtman2@gmail.com>
2017-08-25 09:38:51 -0500

    Fixed Unit Tests to their proper state Added New Unit test to check if NUnit Assert is failing

--- HelloAzureCIUnitTests/ConfigurationSettings.cs
+++ HelloAzureCIUnitTests/ConfigurationSettings.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using NUnit.Framework;
 
 namespace HelloAzureCIUnitTests
@@ -9,16 +10,29 @@ namespace HelloAzureCIUnitTests
         [Test]
         public void Should_not_be_null()
         {
-            string helloTarget = "Azure";//ConfigurationManager.AppSettings["HelloTarget"];
+            string helloTarget = ConfigurationManager.AppSettings["HelloTarget"];
 
             Assert.That(helloTarget, Is.Not.EqualTo(null));
         }
         [Test]
         public void Should_be_specific_to_Azure_environment()
         {
-            string helloTarget = "Azure";//ConfigurationManager.AppSettings["HelloTarget"];
+            string helloTarget = ConfigurationManager.AppSettings["HelloTarget"];
 
             Assert.That(helloTarget, Is.Not.EqualTo("Azure"));
+        }
+
+        [Test]
+        public void Should_run_NUnit_tests_without_any_problems()
+        {
+            try
+            {
+                Assert.That(1, Is.EqualTo(1));
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Failed asserting that 1 Is.EqualTo(1), must be Assert that's failing", e.InnerException);
+            }
         }
     }
 }
