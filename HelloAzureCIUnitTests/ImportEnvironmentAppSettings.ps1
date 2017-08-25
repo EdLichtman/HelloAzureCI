688bd5e0e670c1371d1090cef51da9ca64f672f6
halleonard\elichtman <e.lichtman2@gmail.com>
2017-08-25 17:02:02 -0500

    Missing from last commit

--- HelloAzureCIUnitTests/ImportEnvironmentAppSettings.ps1
+++ HelloAzureCIUnitTests/ImportEnvironmentAppSettings.ps1
@@ -4,28 +4,56 @@ $MainApplicationDir = "$ProjectDir\HelloAzureCI"
 $appSettings = $Env | where-object {$_.Name -contains "APPSETTING"} 
 $connectionStrings = $Env | where-object {$_.Name -contains "CONNECTIONSTRING"} 
 
-#Create xml document
+#AppSettings
 [xml]$appSettingsConfig = New-Object System.Xml.XmlDocument
 $declaration = $appSettingsConfig.CreateXmlDeclaration("1.0", "UTF-8",$null)
-$appSettings = $appSettingsConfig.CreateNode("element", "appSettings", $null)
+$appSettingsNode = $appSettingsConfig.CreateNode("element", "appSettings", $null)
 
 $appSettingsConfig.AppendChild($declaration)
-
-$nameValues = ''
 
 foreach ($appSetting in $appSettings) {
     $key = $appSetting.Name
     $value = $appSetting.Value
-
-    $keyValuePair = $appSettingsConfig.CreateNode("element", "add", $null)
-    $keyValuePair.key = $key
-    $keyValuePair.value = $value
-    
-    $appSettings.AppendChild($keyValuePair)
+    if ($key) {
+        $keyValuePair = $appSettingsConfig.CreateNode("element", "add", $null)
+        $keyValuePair.key = $key
+        $keyValuePair.value = $value
+        
+        $appSettingsNode.AppendChild($keyValuePair)
+    }
 }
 
-$appSettingsConfig.AppendChild($appSettings)
-$appSettingsConfig.save("$MainApplicationDir\App_Data\AppSettings.config")
+$appSettingsConfig.AppendChild($appSettingsNode)
+$appSettingsConfig.save("$MainApplicationDir\App_Data\appSettings.config")
+
+
+#ConnectionStrings
+[xml]$connectionStringsConfig = New-Object System.Xml.XmlDocument
+$declaration = $connectionStringsConfig.CreateXmlDeclaration("1.0", "UTF-8",$null)
+$connectionStringsNode = $appSettingsConfig.CreateNode("element", "connectionStrings", $null)
+
+$connectionStringsConfig.AppendChild($declaration)
+
+foreach ($connectionString in $connectionStrings) {
+    $name = $connectionString.Name
+    $connection = $connectionString.ConnectionString
+    $type = $connectionString.Type
+    if ($connection) {
+        $keyValuePair = $connectionStringsConfig.CreateNode("element", "add", $null)
+        $keyValuePair.key = $key
+        $keyValuePair.value = $value
+        
+        $connectionStringsNode.AppendChild($keyValuePair)
+    }
+}
+
+$connectionStringsConfig.AppendChild($connectionStringsNode)
+$connectionStringsConfig.save("$MainApplicationDir\App_Data\connectionStrings.config")
+
+
+
+
+
 # $connectionStringValues = ''
 # foreach ($connectionString in $connectionStrings) {
 #     $name = $connectionString.Name
