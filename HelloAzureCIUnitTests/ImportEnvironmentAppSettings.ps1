72979a74d8c3b583d3d9b5cb1bf3fb6fade8e95b
halleonard\elichtman <e.lichtman2@gmail.com>
2017-08-25 16:25:58 -0500

    Unloads commented code

--- HelloAzureCIUnitTests/ImportEnvironmentAppSettings.ps1
+++ HelloAzureCIUnitTests/ImportEnvironmentAppSettings.ps1
@@ -1,49 +1,48 @@
-write-host Get-ChildItem Env
-# $ProjectDir = $Env:DEPLOYMENT_SOURCE
-# $UnitTestsDir = "$ProjectDir\HelloAzureCIUnitTests"
+$ProjectDir = $Env:DEPLOYMENT_SOURCE
+$UnitTestsDir = "$ProjectDir\HelloAzureCIUnitTests"
 
-# $app = Get-AzureRmWebApp -Name 
-# $appSettings = $app.siteconfig.AppSettings
-# $connectionStrings = $app.siteconfig.ConnectionStrings
+$app = Get-AzureRmWebApp -Name ELTestAzureCI
+$appSettings = $app.siteconfig.AppSettings
+$connectionStrings = $app.siteconfig.ConnectionStrings
 
-# $nameValues = ''
+$nameValues = ''
 
-# foreach ($appSetting in $appSettings) {
-#     $key = $appSetting.Name
-#     $value = $appSetting.Value
+foreach ($appSetting in $appSettings) {
+    $key = $appSetting.Name
+    $value = $appSetting.Value
 
-#     $nameValues += @"
-#     <add key="$key" value="$value" />
+    $nameValues += @"
+    <add key="$key" value="$value" />
 
-# "@
+"@
 
-# }
+}
 
-# $appSettings =@"
-# <?xml version="1.0" encoding="utf-8" ?>
-# <appSettings>
-# $nameValues
-# </appSettings>
-# "@
+$appSettings =@"
+<?xml version="1.0" encoding="utf-8" ?>
+<appSettings>
+$nameValues
+</appSettings>
+"@
 
-# $connectionStringValues = ''
-# foreach ($connectionString in $connectionStrings) {
-#     $name = $connectionString.Name
-#     $connection = $connectionString.ConnectionString
-#     $type = $connectionString.Type
+$connectionStringValues = ''
+foreach ($connectionString in $connectionStrings) {
+    $name = $connectionString.Name
+    $connection = $connectionString.ConnectionString
+    $type = $connectionString.Type
 
-#     $connectionStringValues += @"
-#     <add name="$name" connectionString="$connection" />
+    $connectionStringValues += @"
+    <add name="$name" connectionString="$connection" />
 
-# "@
-# }
+"@
+}
 
-# $connectionStrings =@"
-# <?xml version="1.0" encoding="utf-8" ?>
-# <connectionStrings>
-# $connectionStringValues
-# </connectionStrings>
-# "@
+$connectionStrings =@"
+<?xml version="1.0" encoding="utf-8" ?>
+<connectionStrings>
+$connectionStringValues
+</connectionStrings>
+"@
 
-# $appSettings | out-file -filepath "$UnitTestsDir\App_Data\AppSettings.config"
-# $connectionStrings | out-file -filepath "$UnitTestsDir\App_Data\connectionStrings.config"
+$appSettings | out-file -filepath "$UnitTestsDir\App_Data\AppSettings.config"
+$connectionStrings | out-file -filepath "$UnitTestsDir\App_Data\connectionStrings.config"
