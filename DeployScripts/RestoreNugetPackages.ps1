c4a7cf6c47686274873695003ee7bd0b7a1fccf8
halleonard\elichtman <e.lichtman2@gmail.com>
2017-08-29 12:57:51 -0500

    Uncommented Nuget restore

--- DeployScripts/RestoreNugetPackages.ps1
+++ DeployScripts/RestoreNugetPackages.ps1
@@ -7,7 +7,7 @@ foreach ($SolutionFile in $AllSolutionFiles) {
     write-Output $SolutionFile
     write-output Hello World
     write-output "About to restore "$SolutionFile.FullName
-    # & nuget restore $SolutionFile.FullName
+    & nuget restore $SolutionFile.FullName
     write-output "Finished restoring $SolutionFile"
 }
 write-output "end of nugetrestore"
