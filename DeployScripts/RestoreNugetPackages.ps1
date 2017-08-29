77665ab56aa833a662b9c630a55a898c57ecf305
halleonard\elichtman <e.lichtman2@gmail.com>
2017-08-29 15:46:47 -0500

    Tests if working without Job

--- DeployScripts/RestoreNugetPackages.ps1
+++ DeployScripts/RestoreNugetPackages.ps1
@@ -3,10 +3,11 @@ $MainSolutionDir = $Env:DEPLOYMENT_SOURCE
 $AllSolutionFiles = Get-ChildItem -path "$MainSolutionDir" -recurse -Include *.sln
 foreach ($SolutionFile in $AllSolutionFiles) {
     $SolutionFileExecutablePath = $SolutionFile.FullName
-    Start-Job -Name RunNugetCommand -Scriptblock {param($SolutionFileExecutablePath)
+    #Start-Job -Name RunNugetCommand -Scriptblock {param($SolutionFileExecutablePath)
         & nuget restore "$SolutionFileExecutablePath" 2>&1 | Out-Null
-        write-output $lastExitCode
-    } -Arg "$SolutionFileExecutablePath"
+     #   write-output $lastExitCode
+     $ErrorLevel = $lastExitCode
+    #} -Arg "$SolutionFileExecutablePath"
     $ErrorLevel = Get-Job -Name RunNugetCommand | Wait-Job | Receive-Job
 
     write-output "Recieving Job"
