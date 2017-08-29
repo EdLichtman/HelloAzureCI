4b49a4ec74f869ffcfd5156205413b98c3c157c3
halleonard\elichtman <e.lichtman2@gmail.com>
2017-08-29 16:04:46 -0500

    Refactors logic of NugetCommand into function Remove Unused cmd file

--- DeployScripts/RestoreNugetPackages.ps1
+++ DeployScripts/RestoreNugetPackages.ps1
@@ -1,17 +1,20 @@
+function NugetRestore-Solution {
+    param ([string] $SolutionExecutablePath)
+    Start-Job -Name RunNugetCommand -Scriptblock {param($sln)
+        & nuget restore "$sln" 2>&1 | Out-Null
+        write-output $lastExitCode
+    } -Arg "$SolutionExecutablePath" | Out-Null
+    $ErrorLevel = Get-Job -Name RunNugetCommand | Wait-Job | Receive-Job 
+
+    return $ErrorLevel
+}
+
 $MainSolutionDir = $Env:DEPLOYMENT_SOURCE
 
 $AllSolutionFiles = Get-ChildItem -path "$MainSolutionDir" -recurse -Include *.sln
 foreach ($SolutionFile in $AllSolutionFiles) {
-    $SolutionFileExecutablePath = $SolutionFile.FullName
-    #Start-Job -Name RunNugetCommand -Scriptblock {param($SolutionFileExecutablePath)
-        & nuget restore "$SolutionFileExecutablePath" 2>&1 | Out-Null
-     #   write-output $lastExitCode
-     $ErrorLevel = $lastExitCode
-    #} -Arg "$SolutionFileExecutablePath"
-    #$ErrorLevel = Get-Job -Name RunNugetCommand | Wait-Job | Receive-Job
-
-    write-output "Recieving Job"
-    write-output $ErrorLevel
+    $ErrorLevel = NugetRestore-Solution $SolutionFile.FullName
+    
     if ($ErrorLevel -ne 0) { 
         write-output "exiting with error "$ErrorLevel
         throw $ErrorLevel
