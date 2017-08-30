c697d3f0591b8068669d4b680137e5c8d132b730
halleonard\elichtman <e.lichtman2@gmail.com>
2017-08-30 08:29:57 -0500

    Broke again at the most unexpected place

--- DeployScripts/RunNUnitTests.ps1
+++ DeployScripts/RunNUnitTests.ps1
@@ -13,9 +13,12 @@ NugetInstall-Package "NUnit.ConsoleRunner" $nUnitVersion $PackagesDir
 Write-Output "Line 11"
 & more "$UnitTestsDir\App_Data\appSettings.config"
 & more "$UnitTestsDir\App_Data\connectionStrings.config"
-
+try {
 $nunit = "$ProjectDir\packages\NUnit.ConsoleRunner.$nUnitVersion\tools\nunit3-console.exe"
 $tests = (Get-ChildItem $OutDir -Recurse -Include *Tests.dll)
+} catch {
+    write-Output $_.Exception.Message
+}
 Write-Output "Line 14 of RunNUnitTests"
 $NUnitTestResults = & $nunit $tests --noheader --framework=$nUnitFramework --work=$OutDir
 Write-Output "Line 16 of RunNUnitTests"
