06930d1557b3953477badc2c2979169ccc58f4b5
halleonard\elichtman <e.lichtman2@gmail.com>
2017-08-31 09:45:22 -0500

    Accidentally pushed import from  local

--- DeployScripts/Functions.ps1
+++ DeployScripts/Functions.ps1
@@ -55,7 +55,6 @@ function Build-DeployableProject {
     Write-Output "`n----- Building $CurrentProjectLocation-----"
 
     $Current_csproj_File = (Get-ChildItem -Path "$CurrentProjectDirectory").Where({$_.Name -Like "*.csproj"}).FullName
-Write-Output $Current_csproj_File
     if ($InPlaceDeployment -ne "1") {
             $arguments = @("$Current_csproj_File"
                             ,"/nologo"
@@ -78,7 +77,6 @@ Write-Output $Current_csproj_File
 
             )
         }
-        write-output "$MSBuild_Path" $arguments
     & "$MSBuild_Path" $arguments
     if ($lastexitcode -ne 0) {
         throw "Could not build $CurrentProjectDirectory"
